58fe0a55169d54a353f551d4fb43e32a0b4d9d38
이민규 <lmg386411@gmail.com>
2024-11-24 19:46:31 +0900

    SWEA D2 1961번 숫자 배열 회전

---
+++ SWEA/D2/1961번 숫자 배열 회전.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+def rotate(arr):
+    arrR = [[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            arrR[i][j] = arr[N-1-j][i]
+    return arrR
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    arr1 = rotate(arr)
+    arr2 = rotate(arr1)
+    arr3 = rotate(arr2)
+
+    print(f"#{tc}")
+    for a, b, c in zip(arr1, arr2, arr3):
+        print(f'{"".join(map(str, a))} {"".join(map(str, b))} {"".join(map(str, c))}')
